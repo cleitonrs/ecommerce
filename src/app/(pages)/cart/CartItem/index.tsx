2ea0d5e2101f7a56a7a5e8c443a730f638c1a00d
cleitonrs <rscleitonrs@gmail.com>
2024-01-01 15:43:18 -0300

    cart page modified

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -6,15 +6,31 @@ import Link from 'next/link'
 
 import { Media } from '../../../_components/Media'
 import { Price } from '../../../_components/Price'
+import { RemoveFromCartButton } from '../../../_components/RemoveFromCartButton'
 
 import classes from './index.module.scss'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
 
-  const decrementQty = () => { }
-  const incrementQty = () => { }
-  const enterQty = () => { }
+  const decrementQty = () => {
+    const updatedQty = quantity > 1 ? quantity - 1 : 1
+
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
+  const incrementQty = () => {
+    const updatedQty = quantity + 1
+
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
+  const enterQty = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedQty = Number(e.target.value)
+
+    setQuantity(updatedQty)
+    addItemToCart({ product, quantity: Number(updatedQty) })
+  }
 
   return (
     <li className={classes.item} key={title}>
@@ -59,6 +75,11 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
             />
           </div>
         </div>
+      </div>
+
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
+        <RemoveFromCartButton product={product} />
       </div>
     </li>
   )
